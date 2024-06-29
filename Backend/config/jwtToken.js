e6491efa052a0166c8ff6f53a8878eb17e8b35aa
mohana-karthik <123633017+mohana-karthik@users.noreply.github.com>
2024-06-29 17:47:51 +0530

    Delete Backend directory

--- Backend/config/jwtToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require ("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, 'secret', { expiresIn: "1d" });
-};
-
-module.exports = { generateToken };
--- Backend/config/refreshtoken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateRefreshToken = (id) => {
-  return jwt.sign({ id }, 'secret', { expiresIn: "1d" });
-};
-
-module.exports = { generateRefreshToken }
